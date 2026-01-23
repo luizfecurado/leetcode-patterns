a169b76a858f346af22f82bf3091441e24f13e32
luizfecurado <luizfecurado@gmail.com>
2026-01-23 20:36:59 -0300

    Two pointers

---
+++ TwoPointers/twopointer.py
@@ -0,0 +1,18 @@
+class Solution:
+    def palavrareversa(s):
+        res = ''
+        l, r = 0,0
+
+        while r < len(s):
+            if s[r] != ' ':
+                r +=1
+            else: 
+                res += s[l:r+1][::-1]
+                r +=1
+                l = r
+        res += ' '
+        res += s[l:r + 2][::-1]
+        return res[1:]
+
+
+print(Solution.palavrareversa("Hello world"))

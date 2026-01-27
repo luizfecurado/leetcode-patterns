885040b71f47c01ed071445b33765e3a5ee44b4f
luizfecurado <luizfecurado@gmail.com>
2026-01-27 20:29:51 -0300

    OOP

---
+++ recap/OOP/exercise4.py
@@ -0,0 +1,23 @@
+class Lampada:
+    
+    def __init__(self,cor,voltagem,luminosidade):
+        self.__cor=cor
+        self.__voltagem=voltagem
+        self.__luminosidade=luminosidade
+        self.__ligada=False
+    
+    def verificar_ligada(self):
+        return self.__ligada
+ 
+    def ligar_desligar(self):
+        if self.__ligada:
+            self.__ligada=False
+            return self.__ligada
+        else:
+            self.__ligada=True
+            return self.__ligada
+            
+lamp1=Lampada('amarela',110,80)
+print(lamp1.verificar_ligada())  #Inicializa desligada
+print(lamp1.ligar_desligar())  #Ligada
+print(lamp1.ligar_desligar())  #Desligada

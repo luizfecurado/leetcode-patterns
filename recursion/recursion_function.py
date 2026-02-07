78317ca2dadf7f5f6a44863fbd4b89f2eda39f3f
luizfecurado <luizfecurado@gmail.com>
2026-02-07 18:15:53 -0300

    Recursion

--- recursion/recursion_function.py
+++ recursion/recursion_function.py
@@ -7,3 +7,11 @@ def regressiva(i):
         regressiva(i-1)
 
 print (regressiva(8))
+
+
+def soma_lista(arr):
+    if len(arr) == 0:
+        return 0
+    return arr[0] + soma_lista(arr[1:])
+
+print(soma_lista([2,4,5,6,3]))

5d15039d2951b0f27bec1de5ffdd747dfc791ebe
luizfecurado <luizfecurado@gmail.com>
2026-02-13 21:26:31 -0300

    Linked List

---
+++ LinkedList/linked_List.py
@@ -0,0 +1,33 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+
+    def print_list(self):
+        current = self.head
+        while current:
+            print(current.data, end="->")
+            current = current.next
+        print("None")
+
+lista = LinkedList()
+lista.add(1)
+lista.add(2)
+lista.add(3)
+lista.print_list()

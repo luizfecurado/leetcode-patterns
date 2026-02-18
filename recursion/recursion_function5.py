5d9eb05ce72e51db851e3533efbfb4c618cc0d9f
luizfecurado <luizfecurado@gmail.com>
2026-02-17 21:14:04 -0300

    Recursion

---
+++ recursion/recursion_function5.py
@@ -0,0 +1,21 @@
+def subsets(nums):
+    result = []
+    
+    def backtrack(index, current_subset):
+        if index == len(nums):
+            result.append(current_subset[:])
+            return
+        
+       
+        current_subset.append(nums[index])
+        backtrack(index + 1, current_subset)
+        
+       
+        current_subset.pop()
+        backtrack(index + 1, current_subset)
+    
+    backtrack(0, [])
+    return result
+
+
+print(subsets([1, 2]))
